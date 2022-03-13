2414604a28884f4cacafb03680fd55fdc6e078ca
Ronald Arias <ariasron@hotmail.com>
2022-03-12 21:44:25 -0500

    feat: fetchs states using the api and displays in a datalist

--- client/js/App.js
+++ client/js/App.js
@@ -13,14 +13,48 @@ class App {
     this.$statesInput.addEventListener("keyup", () => {
       const { value } = this.$statesInput
       if(value.length >= 2) {
-        this.fetchStates(value)
+        fetchStates(value).then(response => this.renderStatesList(response.data))
       }
     })
   }
 
-  fetchStates(inputStates) {
-    console.log(inputStates)
+  /**
+   * Create and render the datalist of states
+   * @param  {Array} states  The states to use for the list
+   */
+  renderStatesList (states) {
+    console.log('states', states)
+    let $dataList = document.createElement('datalist')
+    $dataList.id = 'states-datalist'
+    this.$statesInput.setAttribute('list', $dataList.id)
+
+    let $fragment = document.createDocumentFragment()
+
+    states.forEach(state => {
+      let $option = document.createElement('option')
+      $option.textContent = state.name
+      $fragment.append($option)
+    })
+
+    $dataList.append($fragment)
+
+    this.$statesInput.after($dataList)
   }
 }
 
 new App()
+
+/**
+ * Fetch the states based on the input
+ * @param  {String} searchTerm  The input used to search the states
+ */
+async function fetchStates(searchTerm) {
+  try {
+    const response = await fetch(`http://localhost:3000/api/states?term=${searchTerm}`)
+    const filteredStates = response.json()
+    return filteredStates
+  } catch (err) {
+    console.log('err')
+    throw new Error(err)
+  }
+}
